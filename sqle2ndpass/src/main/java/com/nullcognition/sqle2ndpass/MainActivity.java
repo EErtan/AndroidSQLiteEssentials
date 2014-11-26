5b2b4cf14af1d62fcaa925f314c51e0b9f404dd3
ersin <ersin_the_ertan@hotmail.com>
2014-11-26 01:45:17 -0500

    Chapter 1 - 2nd Pass - Done, code reading and implementation done

--- sqle2ndpass/src/main/java/com/nullcognition/sqle2ndpass/MainActivity.java
+++ sqle2ndpass/src/main/java/com/nullcognition/sqle2ndpass/MainActivity.java
@@ -6,19 +6,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements android.view.View.OnClickListener {
+
+
+   private android.widget.Button insertBtn, readBtn, deleteBtn, updateBtn;
+   private SQLiteHelperClass sqlHelper;
+
 
    @Override
    protected void onCreate(Bundle savedInstanceState){
 	  super.onCreate(savedInstanceState);
-	  setContentView(com.nullcognition.myapplication.R.layout.activity_main);
+	  setContentView(com.nullcognition.sqle2ndpass.R.layout.activity_main);
+
+	  bindViews();
+	  setListeners();
+
+   }
+
+   private void setListeners(){
+	  insertBtn.setOnClickListener(this);
+	  readBtn.setOnClickListener(this);
+	  deleteBtn.setOnClickListener(this);
+	  updateBtn.setOnClickListener(this);
+   }
+
+   private void bindViews(){
+	  insertBtn = (android.widget.Button)findViewById(com.nullcognition.sqle2ndpass.R.id.insertBtn);
+	  readBtn = (android.widget.Button)findViewById(com.nullcognition.sqle2ndpass.R.id.readBtn);
+	  deleteBtn = (android.widget.Button)findViewById(com.nullcognition.sqle2ndpass.R.id.deleteBtn);
+	  updateBtn = (android.widget.Button)findViewById(com.nullcognition.sqle2ndpass.R.id.updateBtn);
    }
 
 
    @Override
    public boolean onCreateOptionsMenu(Menu menu){
 	  // Inflate the menu; this adds items to the action bar if it is present.
-	  getMenuInflater().inflate(com.nullcognition.myapplication.R.menu.menu_main, menu);
+	  getMenuInflater().inflate(com.nullcognition.sqle2ndpass.R.menu.menu_main, menu);
 	  return true;
    }
 
@@ -30,10 +53,32 @@ public class MainActivity extends Activity {
 	  int id = item.getItemId();
 
 	  //noinspection SimplifiableIfStatement
-	  if(id == com.nullcognition.myapplication.R.id.action_settings){
+	  if(id == com.nullcognition.sqle2ndpass.R.id.action_settings){
 		 return true;
 	  }
 
 	  return super.onOptionsItemSelected(item);
+   }
+
+   @Override
+   public void onClick(android.view.View v){
+	  switch(v.getId()){
+		 case com.nullcognition.sqle2ndpass.R.id.insertBtn:
+
+			break;
+		 case com.nullcognition.sqle2ndpass.R.id.readBtn:
+
+			break;
+		 case com.nullcognition.sqle2ndpass.R.id.deleteBtn:
+
+			break;
+		 case com.nullcognition.sqle2ndpass.R.id.updateBtn:
+
+			break;
+		 default:
+			android.util.Log.e("Switch v", "Default parameter invalid");
+			throw new java.security.InvalidParameterException();
+	  }
+
    }
 }
---
+++ sqle2ndpass/src/main/java/com/nullcognition/sqle2ndpass/SQLiteHelperClass.java
@@ -0,0 +1,118 @@
+package com.nullcognition.sqle2ndpass;
+/**
+ * Created by ersin on 26/11/14 at 12:54 AM
+ */
+public class SQLiteHelperClass {
+
+   public static final int    VERSION_NUMBER = 1;
+   public static final String TABLE_CONTACTS = "contacts";
+
+   public static final String KEY_ID     = "_id";
+   public static final String KEY_NAME   = "_name";
+   public static final String KEY_NUMBER = "_primary_number";
+
+   private android.database.sqlite.SQLiteDatabase   db;
+   private android.database.sqlite.SQLiteOpenHelper sqlHelper;
+
+   public SQLiteHelperClass(android.content.Context inContext){
+
+	  sqlHelper = new android.database.sqlite.SQLiteOpenHelper(inContext, "ContactDatabase", null, VERSION_NUMBER) {
+
+		 @Override
+		 public synchronized void close(){
+			super.close();
+			android.util.Log.d("TAG", "Database closed");
+		 }
+
+		 @Override
+		 public void onCreate(android.database.sqlite.SQLiteDatabase db){
+
+			db.execSQL("DROP TABLE IF EXISTS" + TABLE_CONTACTS);
+			onCreate(db);
+		 }
+
+		 @Override
+		 public void onUpgrade(android.database.sqlite.SQLiteDatabase db, int oldVersion, int newVersion){
+
+			String createContactsTable = "CREATE TABLE " +
+										 TABLE_CONTACTS + "(" +
+										 KEY_ID + " INTEGER PRIMANRY KEY," +
+										 KEY_NAME + " TEXT," +
+										 KEY_NUMBER + "INTEGER" + ")";
+
+			try{
+			   db.execSQL(createContactsTable);
+			}
+			catch(android.database.SQLException e){e.printStackTrace();}
+		 }
+
+		 @Override
+		 public void onOpen(android.database.sqlite.SQLiteDatabase db){
+			super.onOpen(db);
+			android.util.Log.d("TAG", "Database opened");
+		 }
+	  };
+   }
+
+   public void insertToSimpleDataBase(){
+
+	  db = sqlHelper.getWritableDatabase();
+
+	  android.content.ContentValues cv = new android.content.ContentValues();
+
+	  cv.put(KEY_NAME, "john");
+	  cv.put(KEY_NUMBER, 00);
+
+	  db.insert(TABLE_CONTACTS, null, cv);
+
+	  cv.put(KEY_NAME, "jack");
+	  cv.put(KEY_NUMBER, 55);
+
+	  db.insert(TABLE_CONTACTS, null, cv);
+   }
+
+   public void getDataFromDatabase(){
+	  int count;
+	  db = sqlHelper.getReadableDatabase();
+	  android.database.Cursor cr = db.query(TABLE_CONTACTS, null, null, null, null, null, null);
+
+	  int id;
+	  if(cr != null){
+		 count = cr.getCount();
+		 cr.moveToFirst();
+		 android.util.Log.d("DATABASE", "count is : " + count);
+		 id = cr.getInt(cr.getColumnIndex(KEY_ID));
+	  }
+
+	  cr = db.rawQuery("select * from " + TABLE_CONTACTS, null);
+	  if(cr != null){
+		 count = cr.getCount();
+		 android.util.Log.d("DATABASE", "count is : " + count);
+	  }
+
+   }
+
+   public void delete(String name){
+
+	  String whereClause = KEY_NAME + "=?";
+	  String[] whereArgs = new String[]{name};
+
+	  db = sqlHelper.getWritableDatabase();
+	  db.delete(TABLE_CONTACTS, whereClause, whereArgs);
+   }
+
+   public void update(String name){
+
+	  String whereClause = KEY_NAME + "=?";
+	  String[] whereArgs = new String[]{name};
+
+	  android.content.ContentValues cv = new android.content.ContentValues();
+
+	  cv.put(KEY_NAME, "Betty");
+	  cv.put(KEY_NUMBER, "999000");
+
+	  db = sqlHelper.getWritableDatabase();
+	  db.update(TABLE_CONTACTS, cv, whereClause, whereArgs);
+   }
+
+}
